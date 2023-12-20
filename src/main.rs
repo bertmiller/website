2e19bee362e4185d12431186a795949ddf1c5d1d
Robert Miller <bert.c.miller@gmail.com>
2023-12-19 19:15:51 -0500

    feat: improve index

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::fs;
 use notify::{Watcher, RecursiveMode, Result as notifyResult};
 use std::sync::mpsc::channel;
 use std::path::Path;
+use chrono::NaiveDate;
 
 fn main() -> notifyResult<()> {
     let (_tx, rx) = channel::<String>();
@@ -12,6 +13,7 @@ fn main() -> notifyResult<()> {
         match res {
             Ok(event) => {
                 println!("{:?}", event);
+                clear_html_files();
                 let md_files = read_markdown_files("./data/");
                 create_html_files(md_files.clone());
                 create_index_page(md_files);
@@ -63,14 +65,55 @@ fn create_html_files(md_files: Vec<String>) {
 // Create an index page
 fn create_index_page(md_files: Vec<String>) {
     let mut index_content = String::from("<ul>");
+    let mut entries: Vec<(String, String, String)> = Vec::new();
+
     for md_file in &md_files {
         let file_name = md_file.replace(".md", ".html");
         let file_name = file_name.replace("data/", "");
-        index_content.push_str(&format!("<li><a href=\"{}\">{}</a></li>", file_name, file_name));
+        let content = fs::read_to_string(&md_file).expect("Error reading file");
+        let first_line = content.lines().next().unwrap_or("");
+        let first_line = first_line.replace("#", "");
+        let second_line = content.lines().nth(1).unwrap_or("");
+        println!("Creating index entry: {}", first_line);
+        println!("Date: {}", second_line);
+        entries.push((file_name, first_line, second_line.to_string()));
     }
+
+    entries.sort_by(|(_, _, a), (_, _, b)| {
+        println!("A: {}", a);
+        println!("B: {}", b);
+        let date_a = NaiveDate::parse_from_str(a, "%m-%d-%Y").unwrap_or_else(|_| NaiveDate::from_ymd(1970, 1, 1));
+        let date_b = NaiveDate::parse_from_str(b, "%m-%d-%Y").unwrap_or_else(|_| NaiveDate::from_ymd(1970, 1, 1));
+        println!("Date A: {}", date_a);
+        println!("Date B: {}", date_b);
+        println!("Date A cmp Date B: {:?}", date_a.cmp(&date_b));
+        date_b.cmp(&date_a)
+    });
+
+    for (file_name, entry, date) in entries {
+        println!("file_name: {}", file_name);   
+        println!("Entry: {}", entry);
+        println!("Date: {}", date);
+        index_content.push_str(&format!("<li><a href=\"{}\">{}</a> - {}</li>", file_name, entry, date));
+    }
+
     index_content.push_str("</ul>");
     index_content = create_html_template("./main.css", &index_content);
     fs::write("./webpage/index.html", index_content).expect("Error writing index file");
+}
+
+// clear old HTML files
+fn clear_html_files() {
+    let pattern = "./webpage/*.html";
+    for entry in glob(pattern).expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path) => {
+                println!("Removing file: {}", path.display());
+                fs::remove_file(path).expect("Error removing file");
+            },
+            Err(e) => println!("{:?}", e),
+        }
+    }
 }
 
 fn create_html_template(css_path: &str, content: &str) -> String {
