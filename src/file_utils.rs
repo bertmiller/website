423a3e70bc23c88eb10a66808210e79c82b20471
Robert Miller <bert.c.miller@gmail.com>
2024-10-26 14:44:28 -0400

    small refactor

--- src/file_utils.rs
+++ src/file_utils.rs
@@ -1,6 +1,38 @@
 use std::fs::create_dir_all;
 use glob::glob;
-use std::{fs, path::Path};
+use std::{fs, path::Path, sync::mpsc::channel};
+use notify::{RecursiveMode, Watcher};
+use crate::config::Config;
+use crate::html_generator;
+
+pub fn create_files(config: &Config, md_files: Vec<String>) {
+    clear_html_files(&config.webpage_dir);
+    html_generator::create_blog_posts(config, md_files.clone());
+    html_generator::create_index_page(config, md_files);
+}
+
+pub fn watch_for_changes(config: &Config) -> Result<(), Box<dyn std::error::Error>> {
+    let (_tx, rx) = channel::<String>();
+
+    let config_clone = config.clone();
+    let mut watcher = notify::recommended_watcher(move |res| match res {
+        Ok(event) => {
+            println!("{:?}", event);
+            let md_files = read_markdown_files(&config_clone.data_dir);
+            create_files(&config_clone, md_files);
+        }
+        Err(e) => println!("watch error: {:?}", e),
+    })?;
+
+    watcher.watch(Path::new(&config.data_dir), RecursiveMode::Recursive)?;
+
+    loop {
+        match rx.recv() {
+            Ok(_) => {}
+            Err(e) => println!("watch receive error: {:?}", e),
+        }
+    }
+}
 
 // Read markdown files from a folder
 pub fn read_markdown_files(folder: &str) -> Vec<String> {
--- src/main.rs
+++ src/main.rs
@@ -4,43 +4,13 @@ mod html_generator;
 
 use config::Config;
 
-use notify::{RecursiveMode, Watcher};
-use std::{path::Path, sync::mpsc::channel};
-
-fn create_files(config: &Config, md_files: Vec<String>) {
-    file_utils::clear_html_files(&config.webpage_dir);
-    html_generator::create_blog_posts(config, md_files.clone());
-    html_generator::create_index_page(config, md_files);
-}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = Config::new()?;
     let md_files = file_utils::read_markdown_files(&config.data_dir);
-    create_files(&config, md_files.clone());
-    watch_for_changes(&config)?;
+    file_utils::create_files(&config, md_files.clone());
+    file_utils::watch_for_changes(&config)?;
 
     Ok(())
 }
 
-fn watch_for_changes(config: &Config) -> Result<(), Box<dyn std::error::Error>> {
-    let (_tx, rx) = channel::<String>();
-
-    let config_clone = config.clone();
-    let mut watcher = notify::recommended_watcher(move |res| match res {
-        Ok(event) => {
-            println!("{:?}", event);
-            let md_files = file_utils::read_markdown_files(&config_clone.data_dir);
-            create_files(&config_clone, md_files);
-        }
-        Err(e) => println!("watch error: {:?}", e),
-    })?;
-
-    watcher.watch(Path::new(&config.data_dir), RecursiveMode::Recursive)?;
-
-    loop {
-        match rx.recv() {
-            Ok(_) => {}
-            Err(e) => println!("watch receive error: {:?}", e),
-        }
-    }
-}
